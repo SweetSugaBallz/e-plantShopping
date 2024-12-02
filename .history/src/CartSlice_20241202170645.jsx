a05120cc9592386155661f662cbc621129be8445
Shannon Dottin <sdot2109@gmail.com>
2024-12-02 17:27:14 -0400

    Updated CartSlice reducers

--- .history/src/CartSlice_20241202170645.jsx
+++ .history/src/CartSlice_20241202170645.jsx
@@ -16,14 +16,10 @@ export const CartSlice = createSlice({
       }    
     },
     removeItem: (state, action) => {
-      state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-      const { name, quantity } = action.payload;
-      const itemToUpdate = state.items.find(item => item.name === name);
-      if (itemToUpdate) {
-        itemToUpdate.quantity = quantity;
-      }    
+
+    
     },
   },
 });
